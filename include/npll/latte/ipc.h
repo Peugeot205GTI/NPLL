2d5ab3b3efdcf4304ee856ebe27f4a1975f97c66
techflashYT <officialTechflashYT@gmail.com>
2025-11-08 22:46:38 -0700

    latte: add linux-loader IPC commands

---
+++ include/npll/latte/ipc.h
@@ -0,0 +1,17 @@
+/*
+ * NPLL - Latte IPC
+ *
+ * Copyright (C) 2025 Techflash
+ *
+ */
+
+#ifndef _LATTE_IPC_H
+
+#define LATTE_IPC_CMD_PRINT 0x01000000
+#define LATTE_IPC_CMD_POWEROFF 0xcafe0001
+#define LATTE_IPC_CMD_REBOOT 0xcafe0002
+#define LATTE_IPC_CMD_MASK 0xff000000
+
+
+#endif /* _LATTE_IPC_H */
+
